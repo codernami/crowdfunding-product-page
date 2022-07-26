7c4dc7370b330798b194834b978b8e742ddfb56f
codernami <codernami@gmail.com>
2022-07-25 22:51:09 -0300

    Change images folder. Adding navbar mobile

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+const openIcon = document.getElementById('icon-open');
+const closeIcon = document.getElementById('icon-close');
+const navMenu = document.getElementById('menu');
+const links = document.getElementsByClassName('nav_item');
+
+
+openIcon.addEventListener('click', () => {
+	navMenu.classList.add('show-menu')
+	closeIcon.classList.toggle('show')
+	openIcon.classList.toggle('hide')
+});
+
+closeIcon.addEventListener('click', () => {
+	navMenu.classList.remove('show-menu')
+	closeIcon.classList.toggle('show')
+	openIcon.classList.toggle('hide')
+});
+
+
+for(let i = 0; i < links.length; i++) {
+	links[i].addEventListener('click', () => {
+		navMenu.classList.remove('show-menu')
+		closeIcon.classList.toggle('show')	
+		openIcon.classList.toggle('hide')
+	})
+};
